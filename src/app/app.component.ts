7ac78349c006e8a260fc3242858ec63f66166da9
StringArrayNull <122176367+stringArrayNull@users.noreply.github.com>
2023-04-05 13:33:54 +0800

    Move setting of env to ngOnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environment/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  env?: string;
   title = 'potpot';
+
+  ngOnInit() {
+    this.env = environment.env;
+  }
 }

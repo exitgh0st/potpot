2e0502be8774a51ee5e32e52827cb3e913ed8ecc
StringArrayNull <122176367+stringArrayNull@users.noreply.github.com>
2023-04-11 16:21:53 +0800

    Update vercel.sh

--- vercel.sh
+++ vercel.sh
@@ -1,7 +1,6 @@
 if [[ $VERCEL_GIT_COMMIT_REF == "master"  ]] ; then 
   npm run build:production
 elif [[ $VERCEL_GIT_COMMIT_REF == "staging"  ]] ; then 
-  echo "This is the staging environment"
   npm run build:staging
 else
   npm run build

4ae0f2accfa8033b3f7d46ee5b5c9017983b65cd
StringArrayNull <122176367+stringArrayNull@users.noreply.github.com>
2023-04-11 16:22:11 +0800

    Add genre and status to anime model

--- src/app/interfaces/anime.ts
+++ src/app/interfaces/anime.ts
@@ -1,5 +1,10 @@
+import { Status } from './status';
+import { Genre } from './genre';
+
 export interface Anime {
-  id: string;
+  id: number;
   title: string;
   synopsis: string;
+  status: Status;
+  genre: Genre;
 }

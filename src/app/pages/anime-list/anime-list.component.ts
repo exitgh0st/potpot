42b6e11f6553b6a6a6db60197fbded576f818b6b
StringArrayNull <122176367+stringArrayNull@users.noreply.github.com>
2023-04-12 23:18:30 +0800

    Add sorting for anime using title and status

--- src/app/pages/anime-list/anime-list.component.ts
+++ src/app/pages/anime-list/anime-list.component.ts
@@ -12,6 +12,12 @@ import { AnimeService } from 'src/app/services/anime.service';
 export class AnimeListComponent {
   animes?: Anime[];
 
+  titleSortOrder?: SortOrder;
+  titleSortIconName: SortArrow = 'arrow_drop_down';
+
+  statusSortOrder?: SortOrder;
+  statusSortIconName: SortArrow = 'arrow_drop_down';
+
   constructor(private animeService: AnimeService) {}
 
   ngOnInit() {
@@ -27,4 +33,71 @@ export class AnimeListComponent {
 
     return genreMap.join(',');
   }
+
+  switchTitleSortOrder() {
+    this.statusSortOrder = undefined;
+
+    if (this.titleSortOrder != SortOrder.ASCENDING) {
+      this.titleSortOrder = SortOrder.ASCENDING;
+    } else {
+      this.titleSortOrder = SortOrder.DESCENDING;
+    }
+
+    this.titleSortIconName = this.titleSortOrder;
+    this.sortByTitle();
+  }
+
+  switchStatusSortOrder() {
+    this.titleSortOrder = undefined;
+
+    if (this.statusSortOrder != SortOrder.ASCENDING) {
+      this.statusSortOrder = SortOrder.ASCENDING;
+    } else {
+      this.statusSortOrder = SortOrder.DESCENDING;
+    }
+
+    this.statusSortIconName = this.statusSortOrder;
+    this.sortByStatus();
+  }
+
+  sortByTitle() {
+    if (!this.animes) {
+      return;
+    }
+
+    switch (this.titleSortOrder) {
+      case SortOrder.ASCENDING:
+        this.animes.sort((anime_a, anime_b) => anime_a.title.localeCompare(anime_b.title));
+        break;
+      case SortOrder.DESCENDING:
+        this.animes.sort((anime_b, anime_a) => anime_a.title.localeCompare(anime_b.title));
+        break;
+    }
+  }
+
+  sortByStatus() {
+    if (!this.animes) {
+      return;
+    }
+
+    switch (this.statusSortOrder) {
+      case SortOrder.ASCENDING:
+        this.animes.sort((anime_a, anime_b) => anime_a.status.value.localeCompare(anime_b.status.value));
+        break;
+      case SortOrder.DESCENDING:
+        this.animes.sort((anime_b, anime_a) => anime_a.status.value.localeCompare(anime_b.status.value));
+        break;
+    }
+  }
 }
+
+export class SortDetail {
+  constructor(private filter: string, private sortOrder: SortOrder) {}
+}
+
+export enum SortOrder {
+  ASCENDING = 'arrow_drop_down',
+  DESCENDING = 'arrow_drop_up'
+}
+
+export type SortArrow = 'arrow_drop_down' | 'arrow_drop_up';
